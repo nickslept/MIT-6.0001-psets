5a0071a8d08f6f636cd79b42d9a3276acaf96a4f
nickslept <nsleptsov@hotmail.com>
2026-04-22 08:08:40 -0400

    psueocode for 4a

--- PSET 4/ps4a.py
+++ PSET 4/ps4a.py
@@ -19,7 +19,21 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    
+    current_first_char = sequence[0]
+    # if length of SEQ == 1 #base case
+        # return [sequence]
+    # if the length of SEQ > 1: (e.g. meats) 
+        #current_permutations = get_permutations(sequence[1:]) #meats --> eats, ats, ts, s; catches the previous permutations
+        #new_permutations = [] #different in each scope
+    # for loop over each word in current permutations (5, 4, 3, 2, 1)
+        # new_permutations.add(current_first_char + word[0:])
+        # for loop from 1 to length of word #might need to be len -1
+            # new_permutations.add(word[0:i] + current_first_char + [i:])
+        # new_permutations.add(word[0:len(word) + current_first_char])
+    # return new_permutations
 
+    
     pass #delete this line and replace with your code here
 
 if __name__ == '__main__':
