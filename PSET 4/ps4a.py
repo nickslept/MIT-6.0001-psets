f6fdfdfc554fc2f0b300a2df7a0b7c8ced505ee2
nickslept <nsleptsov@hotmail.com>
2026-04-22 10:24:52 -0400

    bug fix to get_permutations() and 4 test cases implemented

--- PSET 4/ps4a.py
+++ PSET 4/ps4a.py
@@ -21,16 +21,16 @@ def get_permutations(sequence):
     '''
     
     current_first_char = sequence[0]
-    if len(sequence) == 1: #base case
+    if len(sequence) == 1: #base case (e.g. s in meats)
         return [sequence]
     if len(sequence) > 1: #(e.g. meats) 
         current_permutations = get_permutations(sequence[1:]) #meats --> eats, ats, ts, s; catches the previous permutations
         new_permutations = [] #different in each scope
-    for word in current_permutations: # e.g. when current_first_char in meats in a then curr perms = [ts, st]
-        new_permutations.add(current_first_char + word) #handles placing current_first_char at the beginning of the word
-        for i in range(1, len(word)): #handles placing current_first_char in spots in the middle of the word
-            new_permutations.add(word[0:i] + current_first_char + word[i:])
-        new_permutations.add(word + current_first_char) #handles placing current_first_char at the end of word
+        for word in current_permutations: # e.g. when current_first_char in meats in a then curr perms = [ts, st]
+            new_permutations.append(current_first_char + word) #handles placing current_first_char at the beginning of the word
+            for i in range(1, len(word)): #handles placing current_first_char in spots in the middle of the word
+                new_permutations.append(word[0:i] + current_first_char + word[i:])
+            new_permutations.append(word + current_first_char) #handles placing current_first_char at the end of word
     return new_permutations
 
 
@@ -44,6 +44,17 @@ if __name__ == '__main__':
 #    # Put three example test cases here (for your sanity, limit your inputs
 #    to be three characters or fewer as you will have n! permutations for a 
 #    sequence of length n)
-    
-    pass #delete this line and replace with your code here
+
+    print('Input:', "a")
+    print('Expected Output:', ["a"])
+    print('Actual Output:', get_permutations("a"))
+
+    print('Input:', "ab")
+    print('Expected Output:', ["ab", "ba"])
+    print('Actual Output:', get_permutations("ab"))
+
+    print('Input:', "abc")
+    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+    print('Actual Output:', get_permutations("abc"))
+
 
